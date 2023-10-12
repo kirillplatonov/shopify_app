aa6fcb46565f60eedf84990de9e76a8c0841be8d
Kirill Platonov <kirill@platonov.net>
2023-10-12 15:11:51 +0200

    Do not retry 401 error in WebhooksManagerJob

--- lib/shopify_app/jobs/webhooks_manager_job.rb
+++ lib/shopify_app/jobs/webhooks_manager_job.rb
@@ -10,6 +10,8 @@ module ShopifyApp
       ShopifyAPI::Auth::Session.temp(shop: shop_domain, access_token: shop_token) do |session|
         WebhooksManager.create_webhooks(session: session)
       end
+    rescue ShopifyAPI::Errors::HttpResponseError => error
+      raise error unless error.code == 401
     end
   end
 end
